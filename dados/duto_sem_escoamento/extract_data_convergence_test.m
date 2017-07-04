2ad1560fa9f1694a093abbccabd142e35f617191
José Pedro <1jpsneto@gmail.com>
2017-07-03 22:21:53 -0300

    Arrumando os graficos

--- dados/duto_sem_escoamento/extract_data_convergence_test.m
+++ dados/duto_sem_escoamento/extract_data_convergence_test.m
@@ -24,7 +24,7 @@ result_simulation_interp_abs_r_15(end-1) = mean(result_simulation_interp_abs_r_1
 result_simulation_interp_abs_r_15(end-2) = mean(result_simulation_interp_abs_r_15(end-8:end-5));
 result_simulation_interp_abs_r_15(end-3) = mean(result_simulation_interp_abs_r_15(end-7:end-5));
 result_simulation_interp_abs_r_15(end-4) = mean(result_simulation_interp_abs_r_15(end-6:end-5));
-correlation = corrcoef(result_simulation_interp_abs_r_15, analytical_data_abs_r(:,2));
+correlation = corrcoef(result_simulation_interp_abs_r_15', analytical_data_abs_r(:,2));
 correlation = correlation(1,2)
 
 
