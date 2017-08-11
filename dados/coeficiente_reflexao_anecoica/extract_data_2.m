506645ae0c121a86ec65292c0185385bde243b5e
José Pedro <1jpsneto@gmail.com>
2017-08-10 22:27:20 -0300

    =D

--- dados/coeficiente_reflexao_anecoica/extract_data_2.m
+++ dados/coeficiente_reflexao_anecoica/extract_data_2.m
@@ -29,30 +29,30 @@ D_real = D_real(1:number_downsample:end);
 D_imag = D_imag(1:number_downsample:end); 
 D_abs = D_abs(1:number_downsample:end); 
 
-A_real_1 = [kas(find(kas < 2.5))' A_real(find(kas < 2.5))'];
+A_real_1 = [kas' A_real'];
 save('A_real_1.txt', 'A_real_1', '-ascii', '-double');
-A_imag_1 = [kas(find(kas < 2.5))' A_imag(find(kas < 2.5))'];
+A_imag_1 = [kas' A_imag'];
 save('A_imag_1.txt', 'A_imag_1', '-ascii', '-double');
-A_abs_1 = [kas(find(kas < 2.5))' A_abs(find(kas < 2.5))'];
+A_abs_1 = [kas' A_abs'];
 save('A_abs_1.txt', 'A_abs_1', '-ascii', '-double');
 
-B_real_1 = [kas(find(kas < 2.5))' B_real(find(kas < 2.5))'];
+B_real_1 = [kas' B_real'];
 save('B_real_1.txt', 'B_real_1', '-ascii', '-double');
-B_imag_1 = [kas(find(kas < 2.5))' B_imag(find(kas < 2.5))'];
+B_imag_1 = [kas' B_imag'];
 save('B_imag_1.txt', 'B_imag_1', '-ascii', '-double');
-B_abs_1 = [kas(find(kas < 2.5))' B_abs(find(kas < 2.5))'];
+B_abs_1 = [kas' B_abs'];
 save('B_abs_1.txt', 'B_abs_1', '-ascii', '-double');
 
-C_real_1 = [kas(find(kas < 2.5))' C_real(find(kas < 2.5))'];
+C_real_1 = [kas' C_real'];
 save('C_real_1.txt', 'C_real_1', '-ascii', '-double');
-C_imag_1 = [kas(find(kas < 2.5))' C_imag(find(kas < 2.5))'];
+C_imag_1 = [kas' C_imag'];
 save('C_imag_1.txt', 'C_imag_1', '-ascii', '-double');
-C_abs_1 = [kas(find(kas < 2.5))' C_abs(find(kas < 2.5))'];
+C_abs_1 = [kas' C_abs'];
 save('C_abs_1.txt', 'C_abs_1', '-ascii', '-double');
 
-D_real_1 = [kas(find(kas < 2.5))' D_real(find(kas < 2.5))'];
+D_real_1 = [kas' D_real'];
 save('D_real_1.txt', 'D_real_1', '-ascii', '-double');
-D_imag_1 = [kas(find(kas < 2.5))' D_imag(find(kas < 2.5))'];
+D_imag_1 = [kas' D_imag'];
 save('D_imag_1.txt', 'D_imag_1', '-ascii', '-double');
-D_abs_1 = [kas(find(kas < 2.5))' D_abs(find(kas < 2.5))'];
+D_abs_1 = [kas' D_abs'];
 save('D_abs_1.txt', 'D_abs_1', '-ascii', '-double');
